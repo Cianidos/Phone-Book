d95a44bd3607d9186a8bb1e597c89fc5719e994a
Nikita Alexandrov <nikitosiks338@gmail.com>
2021-11-09 13:26:43 +0300

    Stage 2 bubble sort and jumpSearch

--- Phone Book/task/src/phonebook/Main.kt
+++ Phone Book/task/src/phonebook/Main.kt
@@ -1,6 +1,8 @@
 package phonebook
 
 import java.io.File
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 inline fun <R> measureTime(block: () -> R): Pair<R, Long> {
     val start = System.currentTimeMillis()
@@ -15,8 +17,15 @@ inline fun <R> printMeasured(message: String = "Elapsed", block: () -> R): R {
     return res
 }
 
+infix fun String.splitAt(n: Int) = listOf(take(n), this.drop(n + 1))
+infix fun String.splitAt(ch: Char) = splitAt(indexOf(ch))
+
 fun main() {
-    val dataFile = "C:\\Users\\Ariel\\IdeaProjects\\Phone Book\\directory.txt"
+    val timeFormat = { time: Long ->
+        "%1\$tM min. %1\$tS sec. %1\$tL ms.".format(time)
+    }
+    val dataFile = "C:\\Users\\Ariel\\IdeaProjects\\Phone " +
+            "Book\\directory.txt"
     val findFile = "C:\\Users\\Ariel\\IdeaProjects\\Phone Book\\find.txt"
 
     val data = printMeasured("Reading data file...")
@@ -25,13 +34,60 @@ fun main() {
     val finds = printMeasured("Reading findings file...")
     { File(findFile).readLines() }
 
-    println("Start searching...")
+    println("Start searching (linear search)...")
     val (foundNum, time) = measureTime {
         finds.map { find ->
             data.find { it.contains(find) } != null
         }.count { it }
     }
-
-    val timeFormatted = "%1\$tM min. %1\$tS sec. %1\$tL ms.".format(time)
+    val timeFormatted = timeFormat(time)
     println("Found $foundNum / ${finds.size} entries. Time taken: $timeFormatted")
+
+    println()
+    println("Start searching (bubble sort + jump search)...")
+    val (sorted, sortingTime) = measureTime {
+        data.map { it.splitAt(' ')[1] }.bubbleSort()
+    }
+
+    val (foundJump, jumpTime) = measureTime {
+        finds.map { find ->
+            sorted.jumpSearch(find) != null
+        }.count { it }
+    }
+    val total = timeFormat(sortingTime + jumpTime)
+    println("Found $foundJump / ${finds.size} entries. Time taken: $total")
+    println("Sorting time: ${timeFormat(sortingTime)}")
+    println("Searching time: ${timeFormat(jumpTime)}")
+}
+
+inline fun <reified T : Comparable<T>> List<T>.bubbleSort(
+    cmp: (T, T) -> Boolean = { a, b -> a < b }
+): List<T> {
+    return toMutableList().apply {
+        for (i in indices)
+            for (j in i..lastIndex)
+                if (cmp(get(j), get(i))) {
+                    val tmp = get(j)
+                    set(j, get(i))
+                    set(i, tmp)
+                }
+    }
+}
+
+inline fun <reified T : Comparable<T>> List<T>.jumpSearch(
+    element: T,
+    cmp: (T, T) -> Boolean = { a, b -> a < b }
+): T? {
+    val jumpLen = floor(sqrt(size.toDouble())).toInt()
+    var currIdx = 0
+
+    while (cmp(get(currIdx), element)) {
+        currIdx += jumpLen
+        if (currIdx > lastIndex)
+            return null
+    }
+    while (cmp(element, get(currIdx)))
+        currIdx -= 1
+
+    return get(currIdx)
 }
---
+++ Topics/The Math library/Fourth root/src/Main.kt
@@ -0,0 +1,5 @@
+import kotlin.math.sqrt
+
+fun main() {
+    println(sqrt(sqrt(readLine().orEmpty().toDouble())))
+}
